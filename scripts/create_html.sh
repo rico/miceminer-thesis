6a03074365fb1a262ccc84d79bce3c6a97c70ede
Rico Leuthold <rleuthold@access.ch>
2009-06-09 10:56:55 +0200

    removed latex2html files, since it is not working at all

--- scripts/create_html.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-###########################################
-# create master thesis html with latex2html
-#
-# rleuthold@access.ch - 9.4.2009
-###########################################
-
-/opt/local/bin/latex2html -debug -noreuse -tmp tmp -noinfo -t "Master Thesis miceminer" -contents_in_navigation -address "<a href='mailto:rleuthold@access.ch'>rleuthold@access.ch</a>" -style=style/style.css -dir ../out/html/ src/thesis.tex
